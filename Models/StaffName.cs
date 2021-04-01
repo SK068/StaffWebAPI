fcbe1d7fff26f4e73dfe41f378a8b2b0b7ff2d9e
Supreet Kaur <SK068@atcltd.atc.org.nz>
2021-04-01 13:36:20 +1300

    Add project files.

---
+++ Models/StaffName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffWebAPI.Models
+{
+    public class StaffName
+    {
+        [key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+}
